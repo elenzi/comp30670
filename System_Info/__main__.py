8ac4ed9a7ca80dc6c51fb7ee445099cbd2964562
Elena Lanigan <elenalanigan93@gmail.com>
2018-02-20 10:24:16 +0000

    Updated main

--- System_Info/__main__.py
+++ System_Info/__main__.py
@@ -7,13 +7,13 @@ Created on 1 Feb 2018
 import platform
 
 def main():
+    return platform.platform()
     print("Architecture:", platform.architecture())
     print("OS:", platform.system())
     print("Machine:", platform.machine())
     print("Platform:", platform.platform())
     print("Processor:", platform.processor())
     print("Compiler:", platform.python_compiler())
-    return
     
 if __name__ == '__main__':
     main()
